592c0e011a3e8d16406a78b63e8a8d254873a8e1
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-08-20 11:42:07 -0300

    Update main.py
    Added read_all endpoint

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from typing import Annotated
 from fastapi import Depends, FastAPI
-from .models import Book
+from .models import Books
 from .database import SessionLocal
 from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 def get_db():
-    db = SessionLocal
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get('/books')
+def read_all(db: db_dependency):
+    return db.query(Books).all()
+
