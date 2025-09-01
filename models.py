ce3258ff692a98ed84631fe759eb1781146da059
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-08-31 22:03:16 -0300

    Update models.py
    Added new table to DB

--- models.py
+++ models.py
@@ -11,3 +11,12 @@ class Books(Base):
     author = Column(String)
     status = Column(Boolean, default = True)
     progress = Column(Integer)
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    password = Column(String)
+    role = Column(String)
--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,12 @@
+from datetime import timedelta
 from typing import Annotated
 from fastapi import Depends, APIRouter, HTTPException, Path
 from pydantic import BaseModel, Field
 from models import Books
 from database import Base, SessionLocal, engine
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 router = APIRouter(
     prefix='/auth',
@@ -18,3 +21,7 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+SECRET_KEY = 'Zx7$wR!3M2q#L8uH%@fD9vE&cS0Jp^'
+ALGORITHM = 'HS256'
+
