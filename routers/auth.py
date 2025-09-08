52b905e56bfad8a1a1623201ed1ad0c504ac1744
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-09-07 23:14:40 -0300

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -93,3 +93,16 @@ def create_user(db: db_dependency, new_user_request: UserRequest):
 
     db.add(new_user)
     db.commit()
+
+@router.post('/token', response_model=Token)
+def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                           db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
+    
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
+
+    return {'access_token': token, 'token_type': 'bearer'}
