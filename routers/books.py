a8770d87b62a242a27b4afbb0140211d9bc00ed8
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-09-08 23:50:20 -0300

    Update books.py
    Added user_dependency to all endpoints.

--- routers/books.py
+++ routers/books.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 from models import Books
 from database import Base, SessionLocal, engine
 from sqlalchemy.orm import Session
+from routers.auth import get_current_user
 
 router = APIRouter()
 
@@ -17,6 +18,7 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 class BookRequest(BaseModel):
     title: str = Field(min_length=1, max_length=30)
@@ -25,36 +27,55 @@ class BookRequest(BaseModel):
     progress: int = Field(gt=-1, lt=101)
 
 @router.get('/books', status_code=status.HTTP_200_OK)
-def read_all(db: db_dependency):
+def read_all(db: db_dependency, user: user_dependency):
     """
     Retrive all books for the user.
     """
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='User not authenticated')
+
     return db.query(Books).all()
 
 
 @router.get('/books/{book_id}', status_code=status.HTTP_200_OK)
-def read_by_id(db: db_dependency, book_id: int = Path(gt=0)):
+def read_by_id(db: db_dependency, user: user_dependency, book_id: int = Path(gt=0)):
     """
     Retrive books according to its ID.
     """
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='User not authenticated.')
+
     return db.query(Books).filter(Books.id == book_id).first()
 
 
 @router.post('/books', status_code=status.HTTP_201_CREATED)
-def create_book(db: db_dependency, book_request: BookRequest):
+def create_book(db: db_dependency, user: user_dependency, book_request: BookRequest):
     """
     Create a new book item for the authenticated user.
     """
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='User not authenticated.')
+
     book_model = Books(**book_request.model_dump())
     
     db.add(book_model)
     db.commit()
 
 @router.put('/books/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
-def update_book(db: db_dependency, book_request: BookRequest, book_id: int = Path(gt=0)):
+def update_book(db: db_dependency, 
+                user: user_dependency,
+                book_request: BookRequest, 
+                book_id: int = Path(gt=0)):
     """
     Update the book info according to the ID provided by the user.
     """
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='User not authenticated.')
+
     updated_book = db.query(Books).filter(Books.id == book_id).first()
 
     if updated_book is None:
@@ -68,10 +89,14 @@ def update_book(db: db_dependency, book_request: BookRequest, book_id: int = Pat
     db.commit()
 
 @router.delete('/books/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_book(db: db_dependency, book_id: int = Path(gt=0)):
+def delete_book(db: db_dependency, user: user_dependency, book_id: int = Path(gt=0)):
     """
     Delete book according to the ID provided by the user.
     """
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='User not authenticated.')
+
     book_to_delete = db.query(Books).filter(Books.id == book_id).first()
     
     if book_to_delete is None:
