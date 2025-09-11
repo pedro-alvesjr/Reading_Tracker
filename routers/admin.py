23f6756c45725bf21c904558655ec854c3c8e5f6
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-09-11 18:01:41 -0300

    Create admin.py

---
+++ routers/admin.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+from fastapi import Depends, APIRouter, HTTPException, Path, status
+from pydantic import BaseModel, Field
+from models import Users
+from database import SessionLocal
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from routers.auth import get_current_user
+
+router = APIRouter(
+    prefix='/users',
+    tags=['users']
+)
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
