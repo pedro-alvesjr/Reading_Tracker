d58a72dd5dcc9518f122a6187217279fe13b4040
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-09-16 09:43:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from database import Base, SessionLocal, engine
-from routers import books, auth, users
+from routers import books, auth, users, admin
 import models
 
 app = FastAPI()
@@ -10,3 +10,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(books.router)
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(admin.router)
